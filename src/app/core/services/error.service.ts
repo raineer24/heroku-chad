9e766dc860be7c927a88ad74a34d8d01fe1f5e71
raineer <rain@gmail.com>
2020-07-01 16:17:04 +0800

    foreach loop errors

--- src/app/core/services/error.service.ts
+++ src/app/core/services/error.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from "@angular/core";
 import { AbstractControl, FormGroup } from "@angular/forms/src/model";
+import { isArray } from "util";
 
 @Injectable({ providedIn: "root" })
 export class ErrorService {
   constructor() {}
 
   renderServerErrors(form: FormGroup, response: any) {
-    console.log("error:", response);
+    if (response.status === 422) {
+      const data = response.error.errors;
+      const fields = Object.keys(data || {});
+      data.forEach((elements: any) => {
+        for (const key in elements) {
+          console.log("key", key);
+
+          if (elements.hasOwnProperty(key)) {
+            const element = elements[key];
+            console.log("element", element);
+          }
+        }
+      });
+      // [{ x: 100 }, { x: 200 }, { x: 300 }];
+      // [{ email: "Email is required" }, { password: "Password is required" }]
+    }
   }
 }

7c7843dfc9189ce21066144091d22155609282af
raineer <rain@gmail.com>
2020-07-02 17:38:41 +0800

    formcontrol setfielderror

--- src/app/core/services/error.service.ts
+++ src/app/core/services/error.service.ts
@@ -8,21 +8,42 @@ export class ErrorService {
 
   renderServerErrors(form: FormGroup, response: any) {
     if (response.status === 422) {
-      const data = response.error.errors;
-      const fields = Object.keys(data || {});
-      data.forEach((elements: any) => {
-        for (const key in elements) {
-          console.log("key", key);
+      const data = response.error.errors.errors;
+      console.log("data", data);
 
-          if (elements.hasOwnProperty(key)) {
-            const element = elements[key];
-            console.log("element", element);
-          }
-        }
+      const fields = Object.keys(data || {});
+      console.log("fields", fields);
+
+      data.forEach((elements: any) => {
+        console.log("elements.email", elements.msg);
+        const message = elements.msg;
+        const fieldName = elements.param;
+        //this.setFieldError(form, fieldName, message);
+
+        // for (const key in elements) {
+        //   console.log("key", key);
+        //   console.log("email", key);
+
+        //   if (elements.hasOwnProperty(key)) {
+        //     const element = elements[key];
+        //     console.log("element", element);
+        //   }
+        // }
       });
       // [{ x: 100 }, { x: 200 }, { x: 300 }];
       // [{ email: "Email is required" }, { password: "Password is required" }]
+      //       [
+      //     {
+      //       "type": "field-validation-error",
+      //       "field": "password",
+      //       "message": "Incorrect password"
+      //     }
+      //   ]
     }
+  }
+
+  hasWrongValue(form: FormGroup, fieldName: string) {
+    return this.getFieldErrors(form, fieldName).length > 0;
   }
 
   getFieldError(form: FormGroup, fieldName: string): string {
@@ -31,6 +52,8 @@ export class ErrorService {
 
   getFieldErrors(form: FormGroup, fieldName: string): string[] {
     const control = this.findFieldControl(form, fieldName);
+    console.log("control", control);
+
     if (control && control.touched && control.errors) {
       return this.getErrors(control);
     } else {
@@ -49,6 +72,8 @@ export class ErrorService {
 
   private setFieldError(form: FormGroup, fieldName: string, message: string) {
     const control = this.findFieldControl(form, fieldName);
+    const errors = { [message]: true };
+    control.setErrors(errors);
   }
 
   private findFieldControl(

b18ad4431b4ef1de713c2560c282b764972f999f
raineer <rain@gmail.com>
2020-11-25 13:36:55 +0800

    app state

--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -1,6 +1,7 @@
-import { UsersState } from "../admin/state/user.reducer";
-import { createEntityAdapter, EntityAdapter, EntityState } from "@ngrx/entity";
+// import { UsersState } from "../admin/state/user.reducer";
+// import { createEntityAdapter, EntityAdapter, EntityState } from "@ngrx/entity";
 
-export interface State {
-  users: UsersState;
-}
+// export interface State {
+//   users: UsersState;
+// }
+export interface AppState {}

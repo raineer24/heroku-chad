f5ec469258a6d73b0461b874cfb727f9add928a1
raineer <rain@gmail.com>
2021-08-10 16:56:25 +0800

    logIn effect

--- src/app/store/effects/auth.effects.ts
+++ src/app/store/effects/auth.effects.ts
@@ -14,3 +14,44 @@ import {
   LogInFailure,
   LogInSuccess,
 } from "../actions/auth.actions";
+
+@Injectable()
+export class AuthEffects {
+  constructor(
+    private actions: Actions,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  @Effect()
+  LogIn: Observable<any> = this.actions
+    .pipe(ofType(AuthActionTypes.LOGIN))
+    .map((action: LogIn) => action.payload)
+    .switchMap((payload) => {
+      return this.authService
+        .logIn(payload.email, payload.password)
+        .map((user) => {
+          return new LogInSuccess(user);
+        })
+        .catch((error) => {
+          return Observable.of(new LogInFailure({ error: error }));
+        });
+    });
+
+  @Effect({ dispatch: false })
+  LogInSuccess: Observable<any> = this.actions.pipe(
+    ofType(AuthActionTypes.LOGIN_SUCCESS),
+    tap((user) => {
+      this.authService.setToken(user.payload.token);
+      this.router.navigateByUrl("/");
+    })
+  );
+
+  @Effect({ dispatch: false })
+  LogInFailure: Observable<any> = this.actions.pipe(
+    ofType(AuthActionTypes.LOGIN_FAILURE),
+    tap(() => {
+      this.notifyService.showError("Email/mot de passe incorrectes", "Erreur");
+    })
+  );
+}

dec639bc6ebdaada7decc6a969c182089223ce9b
raineer <rain@gmail.com>
2021-08-29 14:24:45 +0800

    debounceTime

--- src/app/store/effects/user.effects.ts
+++ src/app/store/effects/user.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
-import { Actions, Effect, ofType } from "@ngrx/effects";
+import { act, Actions, Effect, ofType } from "@ngrx/effects";
 import {
   mergeMap,
   switchMap,
@@ -12,11 +12,10 @@ import {
   take,
   withLatestFrom,
 } from "rxjs/operators";
-import { Action, Store} from '@ngrx/store';
+import { Action, Store } from "@ngrx/store";
 import { Observable, of } from "rxjs";
 import { AuthService } from "../../core/services/user.service";
-import {  State } from '../reducers/user.reducer';
-
+import { State } from "../reducers/user.reducer";
 
 import * as userInfoActions from "../actions/user.actions";
 
@@ -26,75 +25,57 @@ export class AuthEffects {
     private actions: Actions,
     private authService: AuthService,
     private router: Router,
-    private store: Store<State>,
+    private store: Store<State>
   ) {}
 
-//   @Effect({ dispatch: false })
-//   loadProfile$: Observable<any> = this.actions$.pipe(
-//     ofType(AuthActions.AuthActionTypes.LOAD_PROFILE_BEGIN),
+  //   @Effect({ dispatch: false })
+  //   loadProfile$: Observable<any> = this.actions$.pipe(
+  //     ofType(AuthActions.AuthActionTypes.LOAD_PROFILE_BEGIN),
 
-//     switchMap(() => {
-//       return this.authService.currentUser.pipe(
-//         take(1),
-//         map((data) => {
-//           console.log("map effect");
-//           return new AuthActions.LoadProfileSuccess(data);
-//           //return new userActions.LoadProfileSuccess(data["user"]);
-//         }),
-//         catchError((error) => of(new AuthActions.LoadProfileFailure(error)))
-//       );
-//     })
-//   );
+  //     switchMap(() => {
+  //       return this.authService.currentUser.pipe(
+  //         take(1),
+  //         map((data) => {
+  //           console.log("map effect");
+  //           return new AuthActions.LoadProfileSuccess(data);
+  //           //return new userActions.LoadProfileSuccess(data["user"]);
+  //         }),
+  //         catchError((error) => of(new AuthActions.LoadProfileFailure(error)))
+  //       );
+  //     })
+  //   );
 
-//   getDoc$ = createEffect(() => {
-//     return this.actions$.pipe(
-//       ofType(fromDocs.getDocument),
-//       switchMap(action =>
-//         of(action).pipe(
-//           withLatestFrom(
-//             this.store.select(fromDocs.getById, { id: action.payload.id })
-//           ),
-//           map(([action, latest]) => {
-//             return fromDocs.someAction();
-//           })
-//         )
-//       )
-//     );
-//   });
+  //   getDoc$ = createEffect(() => {
+  //     return this.actions$.pipe(
+  //       ofType(fromDocs.getDocument),
+  //       switchMap(action =>
+  //         of(action).pipe(
+  //           withLatestFrom(
+  //             this.store.select(fromDocs.getById, { id: action.payload.id })
+  //           ),
+  //           map(([action, latest]) => {
+  //             return fromDocs.someAction();
+  //           })
+  //         )
+  //       )
+  //     );
+  //   });
 
-  @Effect() onGetUserInfo$ = this.actions.pipe(
-      ofType(userInfoActions.UserActionTypes.GET_USER),
-      switchMap(action => of(action).pipe(
-          withLatestFrom(this.store.select((u => u["user"])))
-      ))
-  )
-    // .ofType<userInfoActions.GetUserAction>(
-    //   userInfoActions.UserActionTypes.GET_USER
-    // )
-    
-    mergeMap(([action, state]) => {
-      // If Username Is not changed dont fetch data
-      if (state.userInfo.userInfo.username === action.payload.userName) {
-        return Observable.of({ type: "NO_ACTION" });
-      }
-      return this.userInfoService
-        .GetUserInfo(action.payload.userName)
-        .concatMap((data) => {
-          this.seoService.updateUserPage(data.value);
-          return Observable.from([
-            { type: "CLEAR_PROFILE_STATE" },
-            new userInfoActions.GetUserSuccessAction(data.value),
-            new interestActions.GetInterestAction({
-              userName: action.payload.userName,
-            }),
-          ]);
-        })
-        .catch((error) => {
-          return Observable.of(
-            new userInfoActions.GetUserFailAction({ showError: true })
-          );
-        });
-    });
+  @Effect()
+  getInventory$ = this.actions$.pipe(
+    ofType(GET_INVENTORY),
+    mergeMap((action) =>
+      combineLatest(
+        of(action),
+        this.store$.pipe(
+          select(getIsStoreInventoryLoaded, { branchId: action.branchId })
+        )
+      )
+    ),
+    tap(([action, loaded]) => {
+      // The rest of your code...
+    })
+  );
 
   //   @Effect() onGetUserInfo$ = this.actions$
   //     .ofType<userInfoActions.GetUserAction>(

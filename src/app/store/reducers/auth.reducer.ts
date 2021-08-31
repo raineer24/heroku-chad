b1fa03457df517f0992bfcf4b101a2f78811a8a8
raineer <rain@gmail.com>
2021-08-31 18:39:47 +0800

    create Reducer

--- src/app/store/reducers/auth.reducer.ts
+++ src/app/store/reducers/auth.reducer.ts
@@ -1,49 +1,54 @@
 import { User } from "../../core/models";
 import { All, AuthActionTypes } from "../actions/auth.actions";
+import { createReducer, on, Action } from "@ngrx/store";
 import { UserActionTypes } from "../actions/user.actions";
 
-export interface State {
+export interface AuthModuleState {
+  auth: AuthState;
+}
+
+export interface AuthState {
   isAuthenticated: boolean;
   user: User | null;
 }
 
-export const initialState: State = {
+export const initialState: AuthState = {
   isAuthenticated: false,
   user: null,
 };
 
-export function reducer(state = initialState, action: All): State {
-  switch (action.type) {
-    case AuthActionTypes.LOGIN_SUCCESS: {
-      return {
-        ...state,
-        isAuthenticated: true,
-        user: action.payload,
-      };
-    }
-    case AuthActionTypes.LOGIN_FAILURE: {
-      return {
-        ...state,
-      };
-    }
+// export function reducer(state = initialState, action: All): State {
+//   switch (action.type) {
+//     case AuthActionTypes.LOGIN_SUCCESS: {
+//       return {
+//         ...state,
+//         isAuthenticated: true,
+//         user: action.payload,
+//       };
+//     }
+//     case AuthActionTypes.LOGIN_FAILURE: {
+//       return {
+//         ...state,
+//       };
+//     }
 
-    case AuthActionTypes.LOGOUT: {
-      return initialState;
-    }
-    case AuthActionTypes.SIGNUP_SUCCESS: {
-      return {
-        ...state,
-      };
-    }
-    case AuthActionTypes.SIGNUP_FAILURE: {
-      console.log(action.payload);
-      return {
-        ...state,
-      };
-    }
+//     case AuthActionTypes.LOGOUT: {
+//       return initialState;
+//     }
+//     case AuthActionTypes.SIGNUP_SUCCESS: {
+//       return {
+//         ...state,
+//       };
+//     }
+//     case AuthActionTypes.SIGNUP_FAILURE: {
+//       console.log(action.payload);
+//       return {
+//         ...state,
+//       };
+//     }
 
-    default: {
-      return state;
-    }
-  }
-}
+//     default: {
+//       return state;
+//     }
+//   }
+// }
